1a49cfc75959da178a94e99e9730bc396dbb2739
francescoquartu50@gmail.com <francesco.quartu@pluribus-one.it>
2024-06-12 23:14:01 +0200

    Discretize the map, and control speed

--- main.lua
+++ main.lua
@@ -1,20 +1,38 @@
 function love.load()
+  -- Player position
   x = 0
   y = 0
+
+  -- Player velocity
+  min_v = 5
+  max_v = 10
+  v = min_v
+
+  -- Size of the tilemap
+  unit = 32
 end
 
 function love.update(dt)
+  -- You can move faster by pressing space bar
+  if love.keyboard.isDown("space") then
+    v = max_v
+  else
+    v = min_v
+  end
+
+  -- Actual movement, by pressing arrow keys
   if love.keyboard.isDown("right") then
-    x = x + 100 * dt
+    x = x + v * dt
   elseif love.keyboard.isDown("left") then
-    x = x - 100 * dt
+    x = x - v * dt
   elseif love.keyboard.isDown("up") then
-    y = y - 100 * dt
+    y = y - v * dt
   elseif love.keyboard.isDown("down") then
-    y = y + 100 * dt
+    y = y + v * dt
   end
 end
 
 function love.draw()
-  love.graphics.rectangle("line", x, y, 100, 150)
+  -- Draw the player
+  love.graphics.rectangle("line", math.floor(x) * unit, math.floor(y) * unit, unit, unit)
 end
