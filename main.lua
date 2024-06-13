1bf51910ab18ff81f33a3d9d17f445f4a57a8db9
FraaQuartu <francescoquartu50@gmail.com>
2024-06-13 23:53:57 +0200

    Finish refactoring: move player code in a file containing its class

--- main.lua
+++ main.lua
@@ -1,48 +1,15 @@
 function love.load()
-  -- Import library
-  vector = require("vector")
-
-  -- Player position
-  player = {}
-  player.pos = vector(0, 0)
-
-  -- Speed limits
-  min_v = 5
-  max_v = 10
-
-  -- Player velocity and direction
-  player.dir = vector(0,0)
-  player.v = min_v
-
-  -- Size of the tilemap
+  Object = require "classic"
+  require "player"
+  player = Player()
+  
   unit = 32
 end
 
 function love.update(dt)
-  -- You can move faster by pressing space bar
-  if love.keyboard.isDown("space") then
-    player.v = max_v
-  else
-    player.v = min_v
-  end
-
-  -- Actual movement, by pressing arrow keys
-  if love.keyboard.isDown("right") then
-    player.dir = vector(1,0)
-  elseif love.keyboard.isDown("left") then
-    player.dir = vector(-1,0)
-  elseif love.keyboard.isDown("up") then
-    player.dir = vector(0,-1)
-  elseif love.keyboard.isDown("down") then
-    player.dir = vector(0,1)
-  else
-    player.dir = vector(0,0)
-  end
-
-  player.pos = player.pos + player.v * player.dir * dt
+  player.update(player,dt)
 end
 
 function love.draw()
-  -- Draw the player
-  love.graphics.rectangle("line", math.floor(player.pos.x) * unit, math.floor(player.pos.y) * unit, unit, unit)
+  player.draw(player,unit)
 end
---
+++ player.lua
@@ -0,0 +1,38 @@
+Player = Object.extend(Object)
+
+function Player.new(self)
+  vector = require("vector")
+  self.pos = vector(0,0)
+  self.dir = vector(0,0)
+  self.min_v = 5
+  self.max_v = 10
+  self.v = 5
+end
+
+function Player.update(self, dt)
+  -- You can move faster by pressing space bar
+  if love.keyboard.isDown("space") then
+    self.v = self.max_v
+  else
+    self.v = self.min_v
+  end
+
+  -- Actual movement, by pressing arrow keys
+  if love.keyboard.isDown("right") then
+    self.dir = vector(1,0)
+  elseif love.keyboard.isDown("left") then
+    self.dir = vector(-1,0)
+  elseif love.keyboard.isDown("up") then
+    self.dir = vector(0,-1)
+  elseif love.keyboard.isDown("down") then
+    self.dir = vector(0,1)
+  else
+    self.dir = vector(0,0)
+  end
+
+  self.pos = self.pos + self.v * self.dir * dt
+end
+
+function Player.draw(self, unit)
+  love.graphics.rectangle("line", math.floor(self.pos.x) * unit, math.floor(self.pos.y) * unit, unit, unit)
+end
